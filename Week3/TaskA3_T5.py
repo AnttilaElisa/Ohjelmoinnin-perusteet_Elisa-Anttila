bb97806c35ca71fc5e70601ea98f4484f1368729
Elisa Anttila <elsu.anttila@gmail.com>
2025-09-30 15:07:59 +0300

    Add files via upload

---
+++ Week3/TaskA3_T5.py
@@ -0,0 +1,28 @@
+print("Program starting.\n")
+
+print("Options:")
+print("1 - Celsius to Fahrenheit")
+print("2 - Fahrenheit to Celsius")
+print("0 - Exit")
+
+choice = int(input("Your choice: "))
+
+if(choice == 1):
+    Celsius = float(input("Insert the amount of Celsius: "))
+    Fahrenheits = Celsius * 1.8 + 32
+    print(f"{round(Celsius, 1)}°C equals to {round(Fahrenheits, 1)}°F")
+    print("\nProgram ending.")
+
+elif(choice == 2):
+    Fahrenheits = float(input("Insert the amount of Fahrenheit: "))
+    Celsius = (Fahrenheits - 32) / 1.8
+    print(f"{round(Fahrenheits, 1)}°F equals to {round(Celsius, 1)}°C")
+    print("\nProgram ending.")
+
+elif(choice == 0):
+    print("Exiting...")
+    print("\nProgram ending.")
+
+else:
+    print("Unknown option.")
+    print("\nProgram ending.")
