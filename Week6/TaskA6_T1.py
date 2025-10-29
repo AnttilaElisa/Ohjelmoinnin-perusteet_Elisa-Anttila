be934c8ffa1a76c359f19b7852cac34cb8c62efb
Elisa Anttila <elsu.anttila@gmail.com>
2025-10-29 10:23:27 +0200

    Update TaskA6_T1.py

--- Week6/TaskA6_T1.py
+++ Week6/TaskA6_T1.py
@@ -20,3 +20,17 @@
 
 # #### END "A6_T1_D1.txt" ####
 #Program ending.
+
+print("Program starting.")
+print("This program can read a file.")
+
+filename = input("Insert filename: ")
+
+file = open(filename, "r")
+print(f'#### START "{filename}" ####')
+content = file.read()
+print(content)
+
+file.close()
+print(f'\n#### END "{filename}" ####')
+print("Program ending.")
